1be8ff04ca3df2e445f57ed03ba842cbbf6f535a
Thomas Mueller <t.muller@cern.ch>
2018-05-09 17:49:00 +0200

    TauPolInterface.cc: use standard ROOT code for boost

--- src/TauPolInterface.cc
+++ src/TauPolInterface.cc
@@ -1,5 +1,9 @@
 #include "TauPolSoftware/TauDecaysInterface/interface/TauPolInterface.h"
+
 #include <iostream>
+
+#include <TVector3.h>
+
 TauPolInterface::TauPolInterface():
   mrho(0.773),
   mpi(0.13957018),
@@ -230,22 +234,10 @@ TauPolInterface::~TauPolInterface(){}
 TLorentzVector 
 TauPolInterface::Boost(TLorentzVector pB, TLorentzVector frame)
    {
-     TMatrixT<double> transform(4,4);
-     TMatrixT<double> result(4,1);
-     TVectorT<double> vec(4); 
-     TVector3 b;
-     if(frame.Vect().Mag()==0){ std::cout<<"TPI Boost is not set, perfrom calculation in the Lab Frame   "<<std::endl; return pB;}
-     if(frame.E()==0){ std::cout<<" Caution: Please check that you perform boost correctly!  " <<std::endl; return pB;} 
-     else   b=frame.Vect()*(1/frame.E());
-     vec(0)  = pB.E();    vec(1)  = pB.Px();
-     vec(2)  = pB.Py();   vec(3)  = pB.Pz();
-     double gamma  = 1/sqrt( 1 - b.Mag2());
-     transform(0,0)=gamma; transform(0,1) =- gamma*b.X() ;  transform(0,2) =  - gamma*b.Y();  transform(0,3) = - gamma*b.Z(); 
-     transform(1,0)=-gamma*b.X(); transform(1,1) =(1+ (gamma-1)*b.X()*b.X()/b.Mag2()) ;  transform(1,2) = ((gamma-1)*b.X()*b.Y()/b.Mag2());  transform(1,3) = ((gamma-1)*b.X()*b.Z()/b.Mag2());
-     transform(2,0)=-gamma*b.Y(); transform(2,1) = ((gamma-1)*b.Y()*b.X()/b.Mag2());  transform(2,2) = (1 + (gamma-1)*b.Y()*b.Y()/b.Mag2());  transform(2,3) =  ((gamma-1)*b.Y()*b.Z()/b.Mag2()); 
-     transform(3,0)=-gamma*b.Z(); transform(3,1) =((gamma-1)*b.Z()*b.X()/b.Mag2()) ;  transform(3,2) = ((gamma-1)*b.Z()*b.Y()/b.Mag2());  transform(3,3) = (1 + (gamma-1)*b.Z()*b.Z()/b.Mag2()); 
-     result=transform*convertToMatrix(vec);
-     return TLorentzVector(result(1,0), result(2,0) ,result(3,0), result(0,0));
+     TVector3 boostVector = frame.BoostVector();
+     TLorentzVector result(pB);
+     result.Boost(boostVector.X(), boostVector.Y(), boostVector.Z());
+     return result;
    }
 
 double
